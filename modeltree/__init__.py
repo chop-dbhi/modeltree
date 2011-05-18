17c7b1da2d3d6f36c97bbf6f99941c07b7dc26b6
Byron Ruth <bruth@codeomics.com>
2011-05-17 21:15:31 -0400

    fixed get_version() to return a semantic version

--- modeltree/__init__.py
+++ modeltree/__init__.py
@@ -1,13 +1,11 @@
 VERSION = (0, 9, 0)
 
 def get_version():
-    version = '%s.%s' % VERSION[:2]
-    if version[2]:
-        version = '%s.%s' % (version, VERSION[2])
+    version = '%s.%s' % VERSION[:3]
     if len(VERSION) > 3:
         if version[3:] == ('alpha', 0):
             version = '%s pre-alpha' % version
         else:
             if VERSION[3] != 'final':
-                version = '%s %s %s' % (version, VERSION[3], VERSION[4])
+                version = '%s%s%s' % (version, VERSION[3], VERSION[4])
     return version
