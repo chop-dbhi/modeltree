5dee392c312e0657eb3bb061dc14e82994d48ce1
Byron Ruth <b@devel.io>
2012-06-03 22:08:01 -0400

    Simplify alias/model/modeltree derivation Any of the three can be passed into model, but only if a model tree is defined (or can be created), will the model be set

--- modeltree/query.py
+++ modeltree/query.py
@@ -1,21 +1,13 @@
 from django.db.models import query
 from django.db.models.sql import RawQuery
-
 from modeltree.tree import trees
 from modeltree.utils import M
 
+
 class ModelTreeQuerySet(query.QuerySet):
-    def __init__(self, alias=None, model=None, query=None, using=None):
-        if alias:
-            modeltree = trees[alias]
-        elif model:
-            modeltree = trees[model]
-        else:
-            modeltree = trees.default
-
-        self.modeltree = modeltree
-
-        model = modeltree.root_model
+    def __init__(self, model=None, query=None, using=None):
+        self.modeltree = trees[model]
+        model = self.modeltree.root_model
         super(ModelTreeQuerySet, self).__init__(model, query, using)
 
     def _filter_or_exclude(self, negate, *args, **kwargs):
