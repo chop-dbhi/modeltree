a8487d718e2cde7ea3332ff612c9740b3aefdc10
Byron Ruth <b@devel.io>
2012-12-14 20:51:49 -0500

    Use which instead of relative path to django-admin.py

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-DJANGO_SETTINGS_MODULE='modeltree.tests.settings' PYTHONPATH=. coverage run ../bin/django-admin.py test modeltree
+DJANGO_SETTINGS_MODULE='modeltree.tests.settings' PYTHONPATH=. coverage run `which django-admin.py` test modeltree
 coverage html
