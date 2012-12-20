86e29c4bdae9bd7c172d8f7af713f194d5502721
Byron Ruth <b@devel.io>
2012-12-20 09:22:50 -0500

    Simplify setup.py, add test script

--- setup.py
+++ setup.py
@@ -1,99 +1,35 @@
-from distutils.core import setup
-from distutils.command.install_data import install_data
-from distutils.command.install import INSTALL_SCHEMES
+from setuptools import setup, find_packages
+import modeltree
 
-import os
-import sys
+kwargs = {
+    # Packages
+    'packages': find_packages(exclude=['tests', '*.tests', '*.tests.*', 'tests.*']),
+    'include_package_data': True,
 
-BASE_PACKAGE = 'modeltree'
+    # Dependencies
+    'install_requires': ['django>=1.3'],
 
-class osx_install_data(install_data):
-    # On MacOS, the platform-specific lib dir is /System/Library/Framework/Python/.../
-    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific fix
-    # for this in distutils.command.install_data#306. It fixes install_lib but not
-    # install_data, which is why we roll our own install_data class.
+    'test_suite': 'test_suite',
 
-    def finalize_options(self):
-        # By the time finalize_options is called, install.install_lib is set to the
-        # fixed directory, so we set the installdir to install_lib. The
-        # install_data class uses ('install_data', 'install_dir') instead.
-        self.set_undefined_options('install', ('install_lib', 'install_dir'))
-        install_data.finalize_options(self)
-
-if sys.platform == "darwin":
-    cmdclasses = {'install_data': osx_install_data}
-else:
-    cmdclasses = {'install_data': install_data}
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk(BASE_PACKAGE):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-        elif dirname in ('tests', 'fixtures'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
-# Small hack for working with bdist_wininst.
-# See http://mail.python.org/pipermail/distutils-sig/2004-August/004134.html
-if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
-    for file_info in data_files:
-        file_info[0] = '\\PURELIB\\%s' % file_info[0]
-
-version = __import__(BASE_PACKAGE).get_version()
-
-setup(
-    version = version,
-    name = 'modeltree',
-    author = 'Byron Ruth',
-    author_email = 'ruthb@email.chop.edu',
-    description = 'Django ORM metaprogramming layer',
-    license = 'BSD',
-    keywords = 'Django meta ORM',
-    url = 'https://github.com/cbmi/modeltree',
-
-    requires = ['django'],
-
-    packages = packages,
-    cmdclass = cmdclasses,
-
-    data_files = data_files,
-    classifiers = [
-        'Development Status :: 4 - Beta',
+    # Metadata
+    'version': modeltree.__version__,
+    'name': 'modeltree',
+    'author': 'Byron Ruth',
+    'author_email': 'b@devel.io',
+    'description': 'Django ORM metaprogramming layer',
+    'license': 'BSD',
+    'keywords': 'Django meta ORM dynamic runtime',
+    'url': 'http://cbmi.github.com/modeltree/',
+    'classifiers': [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
+        'Intended Audience :: Information Technology',
     ],
-)
+}
+
+setup(**kwargs)
