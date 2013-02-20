5a31771d7da556e9ec0b12b04e0d3f8a43baffc3
Byron Ruth <b@devel.io>
2013-02-20 12:43:27 -0500

    Override ModelTreeQuerySet._clone to propagate tree

--- modeltree/query.py
+++ modeltree/query.py
@@ -6,17 +6,31 @@ from modeltree.utils import M
 
 class ModelTreeQuerySet(query.QuerySet):
     def __init__(self, model=None, query=None, using=None):
-        self.modeltree = trees[model]
-        model = self.modeltree.root_model
-        super(ModelTreeQuerySet, self).__init__(model, query, using)
+        self.tree = trees[model]
+        super(ModelTreeQuerySet, self).__init__(self.tree.root_model, query, using)
+
+    # Override to ensure no additional modeltrees are created during clone
+    def _clone(self, klass=None, setup=False, **kwargs):
+        if klass is None:
+            klass = self.__class__
+        query = self.query.clone()
+        if self._sticky_filter:
+            query.filter_is_sticky = True
+        c = klass(model=self.tree, query=query, using=self._db)
+        c._for_write = self._for_write
+        c._prefetch_related_lookups = self._prefetch_related_lookups[:]
+        c.__dict__.update(kwargs)
+        if setup and hasattr(c, '_setup_query'):
+            c._setup_query()
+        return c
 
     def _filter_or_exclude(self, negate, *args, **kwargs):
         return super(ModelTreeQuerySet, self)._filter_or_exclude(negate,
-                M(self.modeltree, *args, **kwargs))
+                M(self.tree, *args, **kwargs))
 
     def select(self, *fields, **kwargs):
         queryset = self._clone()
-        return self.modeltree.add_select(queryset=queryset, *fields, **kwargs)
+        return self.tree.add_select(queryset=queryset, *fields, **kwargs)
 
     def raw(self):
         sql, params = self.query.get_compiler(self.db).as_sql()
