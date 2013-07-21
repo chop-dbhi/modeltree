c4b5caf64060b3933576cb5bf9eb29ab8eb20e9c
Byron Ruth <b@devel.io>
2013-07-21 11:45:15 -0400

    Allow initializing ModelTreeManager with a specific tree
    When two or more trees are defined with the same root model, this
    enables specifying which one to based off of. The model could have
    multiple managers defined using different trees.

--- modeltree/managers.py
+++ modeltree/managers.py
@@ -1,9 +1,14 @@
 from django.db import models
 from modeltree.query import ModelTreeQuerySet
 
+
 class ModelTreeManager(models.Manager):
+    def __init__(self, tree=None, *args, **kwargs):
+        super(ModelTreeManager, self).__init__(*args, **kwargs)
+        self.tree = tree or self.model
+
     def get_query_set(self):
-        return ModelTreeQuerySet(model=self.model, using=self.db)
+        return ModelTreeQuerySet(model=self.tree, using=self.db)
 
     def select(self, *args, **kwargs):
         return self.get_query_set().select(*args, **kwargs)
